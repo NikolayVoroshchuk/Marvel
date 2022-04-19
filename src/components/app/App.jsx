b860db093560fe6b7661c92aef0c580678baff1b
Николай Ворощук <nikolay.voroshchuk98@gmail.com>
2022-04-19 21:22:08 +0300

    upd

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
@@ -14,7 +14,7 @@ const SinglePage = lazy(() => import('../../pages/SinglePage'));
 const App = () => {
 
     return (
-        <Router>
+        <HashRouter>
             <div className="app">
                 <AppHeader />
                 <main>
@@ -37,7 +37,7 @@ const App = () => {
                     </Suspense>
                 </main>
             </div>
-        </Router>
+        </HashRouter>
     )
 }
 
